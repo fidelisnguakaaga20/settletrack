a796679f90b043a776ab3441efdedf5f6b7f0c4e
Nguakaaga Mvendaga <fidelisnguakaaga20@gmail.com>
2026-06-21 19:35:53 +0100

    Fix database engine config for Postgres

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,9 +4,15 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from app.core.config import settings
 
 
+connect_args = (
+    {"check_same_thread": False}
+    if settings.database_url.startswith("sqlite")
+    else {}
+)
+
 engine = create_engine(
     settings.database_url,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(
